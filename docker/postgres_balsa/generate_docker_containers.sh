4d693ddd8bab6b2d6c33cb5021007608e92cbf6f
working <57171759+NLGithubWP@users.noreply.github.com>
2024-10-08 20:35:34 +0800

    s

--- docker/postgres_balsa/generate_docker_containers.sh
+++ docker/postgres_balsa/generate_docker_containers.sh
@@ -1,2 +1,2 @@
 docker build -t "balsa_img" -f Dockerfile__balsa_img__11_8 .
-docker build -t "pg_balsa_img" -f Dockerfile__pg_balsa_img_panda15 .
+docker build -t "pg_balsa_img" -f Dockerfile__pg_balsa_img .
