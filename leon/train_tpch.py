a9dcc8c4055093625ad2f4e163e14979de06c813
working <57171759+NLGithubWP@users.noreply.github.com>
2024-10-11 19:12:51 +0800

    disable wandb

--- leon/train_tpch.py
+++ leon/train_tpch.py
@@ -462,7 +462,7 @@ if __name__ == '__main__':
     trainsqls = load_sql(Ttrainsqllist)
     bestplandata = [[[] for _ in range(20)] for _ in range(len(trainquery))]
     bestplanslist = [[] for _ in range(len(sqls))]
-    iteration_num = 30
+    iteration_num = 2
 
     print("load all sql done")
 
