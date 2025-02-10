ae21be49ea9c03f00c4b44d6ff36c26cfdfc57f6
working <57171759+NLGithubWP@users.noreply.github.com>
2025-02-10 21:41:17 +0800

    Update

--- hybrid_qo/schedule_datashift_v2.sh
+++ hybrid_qo/schedule_datashift_v2.sh
@@ -1,19 +1,19 @@
 mkdir logs && \
 mkdir model && \
-python run_mcts.py --query_file workload/query_join__train.json --train_database imdb_ori --test_database imdb_01v2 && \
+python run_mcts.py --query_file workload/query_sample_train.json --train_database imdb_ori --test_database imdb_01v2 && \
 mv model model_imdb_01v2 && \
 mv logs logs_imdb_01v2 && \
 
 
 mkdir logs && \
 mkdir model && \
-python run_mcts.py --query_file workload/query_join__train.json --train_database imdb_ori --test_database imdb_05v2 && \
+python run_mcts.py --query_file workload/query_sample_train.json --train_database imdb_ori --test_database imdb_05v2 && \
 mv model model_imdb_05v2 && \
 mv logs logs_imdb_05v2 && \
 
 
 mkdir logs && \
 mkdir model && \
-python run_mcts.py --query_file workload/query_join__train.json --train_database imdb_ori --test_database imdb_07v2 && \
+python run_mcts.py --query_file workload/query_sample_train.json --train_database imdb_ori --test_database imdb_07v2 && \
 mv model model_imdb_07v2 && \
 mv logs logs_imdb_07v2
