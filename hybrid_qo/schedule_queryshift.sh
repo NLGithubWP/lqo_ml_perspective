fe62b04cbdb9fddfc48b07fa83a69855eb28625a
working <57171759+NLGithubWP@users.noreply.github.com>
2025-02-25 10:52:18 +0800

    QUery fhi

--- hybrid_qo/schedule_queryshift.sh
+++ hybrid_qo/schedule_queryshift.sh
@@ -9,5 +9,5 @@ mkdir logs && \
 mkdir model && \
 python run_mcts.py --query_file workload/query_sample_join_05__train.json --train_database imdb_ori --test_database imdb_ori && \
 mv model model_imdb_query_shift_05 && \
-mv logs logs_imdb_query_shift_05 && \
+mv logs logs_imdb_query_shift_05
 
