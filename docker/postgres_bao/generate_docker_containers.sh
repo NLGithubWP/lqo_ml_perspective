ae44602b052b29367b4d758396dbd01ec4453eea
working <57171759+NLGithubWP@users.noreply.github.com>
2025-05-06 20:12:59 +0800

    Add preprocessing

--- docker/postgres_bao/generate_docker_containers.sh
+++ docker/postgres_bao/generate_docker_containers.sh
@@ -1,2 +1,4 @@
 docker build -t "pg_bao_img" -f Dockerfile__pg_bao_img .
 docker build -t "bao_server_img" -f Dockerfile__bao_server_img .
+
+docker build -t "bao_server_img_gpu" -f Dockerfile__bao_server_img_gpu .
