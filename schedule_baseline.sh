58dcf32d20d5547f4dde800821de96fbfe70507e
working <57171759+NLGithubWP@users.noreply.github.com>
2025-05-07 15:19:02 +0800

    Update server

--- schedule_baseline.sh
+++ schedule_baseline.sh
@@ -6,7 +6,6 @@ set -e # Exit immediately if a command exits with a non-zero status
 python3 run_test_queries.py --use_bao --query_dir /data/datasets/imdb_queries/ori_queries --output_file log_vldb/test_bao_imdb_ori_query_full.log --database_name imdb_ori
 
 # Default PG on (imdb_13_d_0.1, JOB_full test)
-cd /app/bao
 python3 run_test_queries.py --use_postgres --query_dir /data/datasets/imdb_queries/ori_queries --output_file log_vldb/test_pg_imdb_01v2_query_full.log --database_name imdb_01v2
 
 # Default PG on (imdb_13_d_0.5, JOB_full test)
