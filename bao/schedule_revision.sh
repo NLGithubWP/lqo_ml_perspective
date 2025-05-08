5d9f6401806861add15067d08ab1215d98316b58
Naili <xingnaili14@gmail.com>
2025-05-08 02:51:24 +0000

    update scripts for bao

--- bao/schedule_revision.sh
+++ bao/schedule_revision.sh
@@ -11,23 +11,18 @@ cd /app/bao
 python3 run_queries.py --query_dir /data/datasets/imdb_queries/ori_queries --output_file log_vldb/train_bao_imdb_ori_query_full.log --database_name imdb_ori
 python3 run_test_queries.py --use_bao --query_dir /data/datasets/imdb_queries/ori_queries --output_file log_vldb/test_bao_imdb_ori_query_full.log --database_name imdb_ori
 
-cd /app/bao/bao_server
-cp bao_default_model bao_default_model_imdb_ori_query_full
-cp bao_previous_model bao_previous_model_imdb_ori_query_full
-rm bao.db
-# pkill -f "python3 -u main.py"
-
-
 # (imdb_13, JOB_full train) + (imdb_13_d_0.1, JOB_full test)
-cd /app/bao
 python3 run_test_queries.py --use_bao --query_dir /data/datasets/imdb_queries/ori_queries --output_file log_vldb/test_bao_imdb_01v2_query_full.log --database_name imdb_01v2
 
-
 # (imdb_13, JOB_full train) + (imdb_13_d_0.5, JOB_full test)
-cd /app/bao
 python3 run_test_queries.py --use_bao --query_dir /data/datasets/imdb_queries/ori_queries --output_file log_vldb/test_bao_imdb_05v2_query_full.log --database_name imdb_05v2
 
-
 # (imdb_13, JOB_full train) + (imdb_17, JOB_full test)
-cd /app/bao
 python3 run_test_queries.py --use_bao --query_dir /data/datasets/imdb_queries/ori_queries --output_file log_vldb/test_bao_imdb_17_query_full.log --database_name imdb_17v2
+
+# clean and save the model
+cd /app/bao/bao_server
+mv bao_default_model bao_default_model_imdb_ori_query_full
+mv bao_previous_model bao_previous_model_imdb_ori_query_full
+rm bao.db
+# pkill -f "python3 -u main.py"
---
+++ bao/schedule_revision_arm.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+set -e # Exit immediately if a command exits with a non-zero status
+
+# # 5 arm,
+# cd /app/bao/bao_server
+# nohup python3 -u main.py >./server_train_bao_imdb_ori_query_full_arm_5.log 2>&1 &
+
+# # (imdb_13, JOB_full train) + (imdb_13, JOB_full test)
+# cd /app/bao
+# python3 run_queries.py --query_dir /data/datasets/imdb_queries/ori_queries --output_file log_vldb/train_bao_imdb_ori_query_full_arm_5.log --database_name imdb_ori
+# python3 run_test_queries.py --use_bao --query_dir /data/datasets/imdb_queries/ori_queries --output_file log_vldb/test_bao_imdb_ori_query_full_arm_5.log --database_name imdb_ori
+
+# (imdb_13, JOB_full train) + (imdb_13_d_0.1, JOB_full test)
+# python3 run_test_queries.py --use_bao --query_dir /data/datasets/imdb_queries/ori_queries --output_file log_vldb/test_bao_imdb_01v2_query_full.log --database_name imdb_01v2
+
+# # (imdb_13, JOB_full train) + (imdb_13_d_0.5, JOB_full test)
+# python3 run_test_queries.py --use_bao --query_dir /data/datasets/imdb_queries/ori_queries --output_file log_vldb/test_bao_imdb_05v2_query_full.log --database_name imdb_05v2
+
+# # (imdb_13, JOB_full train) + (imdb_17, JOB_full test)
+# python3 run_test_queries.py --use_bao --query_dir /data/datasets/imdb_queries/ori_queries --output_file log_vldb/test_bao_imdb_17_query_full.log --database_name imdb_17v2
+
+# clean and save the model
+cd /app/bao/bao_server
+mv bao_default_model bao_default_model_imdb_ori_query_full
+mv bao_previous_model bao_previous_model_imdb_ori_query_full
+rm bao.db
+pkill -f "python3 -u main.py"
+
+
+# 10 arm,
+cd /app/bao
+sed -i 's/SET bao_num_arms TO 5/SET bao_num_arms TO 10/g' run_queries.py
+sed -i 's/SET bao_num_arms TO 5/SET bao_num_arms TO 10/g' run_test_queries.py
+
+cd /app/bao/bao_server
+nohup python3 -u main.py >./server_train_bao_imdb_ori_query_full_arm_10.log 2>&1 &
+
+
+# (imdb_13, JOB_full train) + (imdb_13, JOB_full test)
+cd /app/bao
+python3 run_queries.py --query_dir /data/datasets/imdb_queries/ori_queries --output_file log_vldb/train_bao_imdb_ori_query_full_arm_10.log --database_name imdb_ori
+python3 run_test_queries.py --use_bao --query_dir /data/datasets/imdb_queries/ori_queries --output_file log_vldb/test_bao_imdb_ori_query_full_arm_10.log --database_name imdb_ori
+
+cd /app/bao/bao_server
+mv bao_default_model bao_default_model_imdb_ori_query_full_arm_10
+mv bao_previous_model bao_previous_model_imdb_ori_query_full_arm_10
+rm bao.db
+
+cd /app/bao/bao_server
+pkill -f "python3 -u main.py"
+
+
+
+# 15 arm,
+cd /app/bao
+sed -i 's/SET bao_num_arms TO 10/SET bao_num_arms TO 15/g' run_queries.py
+sed -i 's/SET bao_num_arms TO 10/SET bao_num_arms TO 15/g' run_test_queries.py
+
+cd /app/bao/bao_server
+nohup python3 -u main.py >./server_train_bao_imdb_ori_query_full_arm_15.log 2>&1 &
+
+
+# (imdb_13, JOB_full train) + (imdb_13, JOB_full test)
+cd /app/bao
+python3 run_queries.py --query_dir /data/datasets/imdb_queries/ori_queries --output_file log_vldb/train_bao_imdb_ori_query_full_arm_15.log --database_name imdb_ori
+python3 run_test_queries.py --use_bao --query_dir /data/datasets/imdb_queries/ori_queries --output_file log_vldb/test_bao_imdb_ori_query_full_arm_15.log --database_name imdb_ori
+
+cd /app/bao/bao_server
+mv bao_default_model bao_default_model_imdb_ori_query_full_arm_15
+mv bao_previous_model bao_previous_model_imdb_ori_query_full_arm_15
+rm bao.db
+
+cd /app/bao/bao_server
+pkill -f "python3 -u main.py"
+
+
+# 20 arm,
+cd /app/bao
+sed -i 's/SET bao_num_arms TO 15/SET bao_num_arms TO 20/g' run_queries.py
+sed -i 's/SET bao_num_arms TO 15/SET bao_num_arms TO 20/g' run_test_queries.py
+
+cd /app/bao/bao_server
+nohup python3 -u main.py >./server_train_bao_imdb_ori_query_full_arm_20.log 2>&1 &
+
+
+# (imdb_13, JOB_full train) + (imdb_13, JOB_full test)
+cd /app/bao
+python3 run_queries.py --query_dir /data/datasets/imdb_queries/ori_queries --output_file log_vldb/train_bao_imdb_ori_query_full_arm_20.log --database_name imdb_ori
+python3 run_test_queries.py --use_bao --query_dir /data/datasets/imdb_queries/ori_queries --output_file log_vldb/test_bao_imdb_ori_query_full_arm_20.log --database_name imdb_ori
+
+cd /app/bao/bao_server
+mv bao_default_model bao_default_model_imdb_ori_query_full_arm_20
+mv bao_previous_model bao_previous_model_imdb_ori_query_full_arm_20
+rm bao.db
+
+cd /app/bao/bao_server
+pkill -f "python3 -u main.py"
+
+
+# Reset
+cd /app/bao
+sed -i 's/SET bao_num_arms TO 20/SET bao_num_arms TO 5/g' run_queries.py
+sed -i 's/SET bao_num_arms TO 20/SET bao_num_arms TO 5/g' run_test_queries.py
