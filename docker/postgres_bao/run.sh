bdcc36e61eddf00332018c677647f4698748e066
working <57171759+NLGithubWP@users.noreply.github.com>
2025-05-08 15:46:02 +0800

    Update server

--- docker/postgres_bao/run.sh
+++ docker/postgres_bao/run.sh
@@ -53,3 +53,19 @@ docker run --gpus all -d \
   --dns 8.8.8.8 \
   bao_server_img_gpu \
   tail -f /dev/null
+
+
+
+# run the server docker
+docker run --gpus all -d \
+  --name balsa_server \
+  --network balsa_network \
+  --ip 10.6.0.4 \
+  -e POSTGRES_DB=imdbload \
+  -e POSTGRES_USER=postgres \
+  -e POSTGRES_PASSWORD=postgres \
+  -v $(pwd)/../../../lqo_ml_perspective:/app/lqo_ml_perspective \
+  -v $(pwd)/../../../../../datasets:/data/datasets \
+  --shm-size=10g \
+  --dns 8.8.8.8 \
+  balsa_img
