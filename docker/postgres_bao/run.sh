01f7de3dee238a6cfd47ad496a5bcb0205e7e19d
working <57171759+NLGithubWP@users.noreply.github.com>
2025-05-08 18:10:15 +0800

    Update server

--- docker/postgres_bao/run.sh
+++ docker/postgres_bao/run.sh
@@ -32,20 +32,15 @@ docker start pg_bao
 # run the server docker
 docker run --gpus all -d \
   --name bao_server \
-  --network network \
-  --ip 10.5.0.6 \
   -e POSTGRES_DB=imdbload \
   -e POSTGRES_USER=postgres \
   -e POSTGRES_PASSWORD=postgres \
-  -v $(pwd)/../../bao:/app/bao \
-  -v $(pwd)/../../../../../datasets:/data/datasets \
-  -v $(pwd)/install_python_env.sh:/docker-entrypoint-initdb.d/install_python_env.sh \
+  -v ~/AI4QueryOptimizer:/app/AI4QueryOptimizer \
+  -v ~/datasets:/data/datasets \
   -p 9381:9381 \
   --shm-size=2g \
-  --dns 8.8.8.8 \
   bao_server_img_gpu \
   tail -f /dev/null
-
 
 
 # run the server docker for other services
