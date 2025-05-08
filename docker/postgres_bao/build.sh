b31acbb23beaf07e835b091d8947316299714118
working <57171759+NLGithubWP@users.noreply.github.com>
2025-05-08 17:01:30 +0800

    Update server

--- docker/postgres_bao/build.sh
+++ docker/postgres_bao/build.sh
@@ -1,4 +1,6 @@
 docker build -t "pg_bao_img" -f Dockerfile__pg_bao_img .
 docker build -t "bao_server_img_gpu" -f Dockerfile__bao_server_img_gpu .
 
+docker build -t "pg_bao_img_full" -f Dockerfile__pg_bao_img_full .
+
 #docker build -t "bao_server_img" -f Dockerfile__bao_server_img .
