ccf0254a6b5d576ca3952b1720c30bde5462bcdd
working <57171759+NLGithubWP@users.noreply.github.com>
2025-06-10 17:02:05 +0800

    Update config imports

--- balsa/run.py
+++ balsa/run.py
@@ -71,8 +71,6 @@ import train_utils
 import experiments  # noqa # pylint: disable=unused-import
 import experiments_debug
 import exp_job_data_shift
-import exp_job_ceb
-import exp_job_ceb_debug
 import exp_job_light
 import exp_job_light_debug
 
