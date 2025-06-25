db96c686508c5a1e2631689d5a906ee74a2671dc
working <57171759+NLGithubWP@users.noreply.github.com>
2025-06-25 20:29:52 +0800

    Add ports to 54333

--- balsa/run_pg_only.py
+++ balsa/run_pg_only.py
@@ -8,7 +8,7 @@ parser.add_argument("--dbname", type=str, required=True, help="Name of the datab
 args = parser.parse_args()
 
 # Database configuration
-PORT = 5433
+PORT = 54333
 HOST = "localhost"
 USER = "postgres"
 PASSWORD = "postgres"
